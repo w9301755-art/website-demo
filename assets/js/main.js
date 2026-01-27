d94b46c4f9cd63567a707922705119c6a9e0ba61
websitebuilder858 <w9301755@gmail.com>
2026-01-27 20:11:24 +0100

    minor fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,19 @@
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
+  let scrollTimeout;
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+      window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    }, 10);
   }
 
-  document.addEventListener('scroll', toggleScrolled);
+  document.addEventListener('scroll', toggleScrolled, { passive: true });
   window.addEventListener('load', toggleScrolled);
 
   /**
